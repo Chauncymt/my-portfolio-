a2f8e7564761a4da5a3d3c77828a2b5ce50b4b79
Chauncymt <133006733+Chauncymt@users.noreply.github.com>
2023-06-24 22:01:15 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,38 @@
+<?php
+$server = "localhost";
+$username = "root";
+$dBpassword = "N@ruch1g0";
+$dbName = "loginSystem";
+
+$connection = new mysqli($server, $username, $dBpassword, $dbName);
+
+if ($connection->connect_error) {
+    die("Connection error: " . $connection->connect_error);
+} else {
+    echo "Connection ok<br/>";
+}
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $dateOfBirth = $_POST["dateOfBirth"];
+    $levelOfStudy= $_POST["levelOfStudy"];
+    $nickname = $_POST["nickname"];
+
+
+    $sql = "INSERT INTO registration (firstName,SurName, dateOfBirth, level of study,nickname) 
+            VALUES ('$firstName', '$lastName', '$dateOfBirth', '$levelOfStudy', '$nickname')";
+
+
+    if ($connection->query($sql) === true) {
+        echo "User registered successfully.";
+        
+        header("Location: loginPage.php");
+        exit;
+    } else {
+        echo "Error: " . $sql . "<br>" . $connection->error;
+    }
+}
