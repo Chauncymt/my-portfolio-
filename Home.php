8c0f7744efd3b8fa6557a1fc12792bd82dc7d1bb
Chauncymt <133006733+Chauncymt@users.noreply.github.com>
2023-06-25 10:08:48 +0100

    Update Home.php

--- Home.php
+++ Home.php
@@ -48,59 +48,52 @@ border-color:rgb(223, 247, 12);
 }
 </style>
 
-<?php
-$server = "localhost";
-$username = "root";
-$dBpassword = "";
-$dbName = "my_data";
-
-$connection = new mysqli($server, $username, $dBpassword, $dbName);
-
-if ($connection->connect_error) {
-    die("Connection error: " . $connection->connect_error);
-} else {
-    echo "Connection ok<br/>";
-}
-
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $userName = $_POST["dateOfBirth"];
-    $email = $_POST["level of study"];
-    $nickname= $_POST["nickname"];
-    $password = $_POST[""];
-
-
-    $sql = "INSERT INTO user (firstName, lastName, userName, email, password) 
-            VALUES ('$firstName', '$lastName', '$userName', '$email', '$password')";
-
-
-    if ($connection->query($sql) === true) {
-        echo "User registered successfully.";
-        
-        header("Location: home.php");
-        exit;
-    } else {
-        echo "Error: " . $sql . "<br>" . $connection->error;
-    }
-}
-
-
-
-?>
 <body background="istockphoto-820219934-1024x1024.jpg">
 <div class="input-form">
 <form action="register.php" method="post">
 <h1><font size="29px"color="red">  Chauncy Mills Programing school </h1></font>
 <p style="color:rgb(220, 235, 22)">Register with us</p>
-<input type="text" class="name-bt" name="user1stname" id="user1stname" placeholder="First Name">
-<input type="text" class="name-bt" name="user2rdname" id="user2rdname" placeholder="Last Name"></P>
-<input type="text" class="name-bt" name="nickname" id="nickname"  placeholder="Date of birth"></br>
-<input type="text" class="name-bt" name="nickname" id="nickname"  placeholder="Level of study"></br>
-<input type="text" class="name-bt" name="nickname" id="nickname"  placeholder="nickname"></br>
+<form action="insert.php" method="POST">
+  <table>
+   <tr>
+    <td>Name :</td>
+    <td><input type="text" name="username" required></td>
+   </tr>
+   <tr>
+    <td>Password :</td>
+    <td><input type="password" name="password" required></td>
+   </tr>
+   <tr>
+    <td>Gender :</td>
+    <td>
+     <input type="radio" name="gender" value="m" required>Male
+     <input type="radio" name="gender" value="f" required>Female
+    </td>
+   </tr>
+   <tr>
+    <td>Email :</td>
+    <td><input type="email" name="email" required></td>
+   </tr> 
+   <tr>
+    <td>Phone no :</td>
+    <td>
+     <select name="phoneCode" required>
+      <option selected hidden value="">Select Code</option>
+      <option value="977">+265</option>
+      <option value="978">+2657</option>
+      <option value="979">+979</option>
+      <option value="973">+973</option>
+      <option value="972">+972</option>
+      <option value="974">+974</option>
+     </select>
+     <input type="phone" name="phone" required>
+    </td>
+   </tr>
+   <tr>
+    <td><input type="submit" value="Submit" name="submit"></td>
+   </tr>
+  </table>
+ </form>
 <button type="submit" name="button" value="">   submit</button>
 <p><hr /> <font color="white">Make sure you entered correct informatin!</font> <hr /></p>
 <br/>
