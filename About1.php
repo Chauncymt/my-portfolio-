4bf9c9097e53f6b35853a3f1b351baa92c093745
Chauncymt <133006733+Chauncymt@users.noreply.github.com>
2023-06-25 11:25:41 +0100

    Add files via upload

---
+++ About1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html> 
+<head>
+<title>About</title>
+<style>
+    body{
+    background-color:green;
+    align: centre;
+    
+    }
+</style>
+</head>
+<body style="">
+<h2 style="color:red">About the author</h2>
+<p>Am chauncy Mills. I did my secondary education at Umbwi Secobdary School in Dedza. Am currently doing education ICT at Mzuzu University</p>
+<h4 style="color:orange ">Skills and competicies</h4>
+<p>I have knowledge in the following areas:
+<ul>
+    <li>Java programming</li>
+    <li>Programmimg in C </li>
+    <li>Web designing and development(HTML, CSS, JS, PHP, SQL)</li>
+    <li>Networking</li>
+    <li>PC Maintainance</li>
+    <li>Multimedia development (Photography and Videography)</li>
+    <li>Graphics design</li>
+  
+</ul>
+</p>
+</body>
+</html>
